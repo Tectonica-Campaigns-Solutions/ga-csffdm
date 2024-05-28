2ff16bc25724589812a1b34a2c855dce93886819
tectonicafer <fernando@tectonica.co>
2024-05-28 16:06:11 -0300

    remove unnecessary plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,23 +24,12 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
-    {
+    /*{
       resolve: 'gatsby-plugin-netlify',
       options: {
         functionsSrc: `${__dirname}/src/api`,
         functionsOutput: `${__dirname}/functions`,
       },
-    },
-    {
-      resolve: 'gatsby-plugin-mailchimp',
-      options: {
-          endpoint: 'https://tectonica.us18.list-manage.com/subscribe/post?u=b1d4f912bb0367fed3628a0ea&amp;id=00b1bc7a6a&amp;f_id=00d03ee1f0', // string; add your MC list endpoint here; see instructions below
-          timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
-      },
-    },
+    },*/
   ],
-  /*proxy: {
-    prefix: "/.netlify/functions",
-    url: "http://localhost:8000",
-  },*/
 };
